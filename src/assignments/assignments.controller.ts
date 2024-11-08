31d575c5e164195da7869362662c78988fa5dc9f
lelis0210 <themostnefarious@gmail.com>
2024-11-09 01:14:46 +0800

    assignment 2 completed

--- src/assignments/assignments.controller.ts
+++ src/assignments/assignments.controller.ts
@@ -3,21 +3,15 @@ import { Controller, Get, Param } from '@nestjs/common';
 @Controller('assignments')
 export class AssignmentsController {
 
-    @Get('fibonacci/:n')
-    getFibonacciSequence(@Param('n') n: string): object {
-        const num = parseInt(n);
-        if (isNaN(num) || num <= 0) {
-            return { message: 'Please provide a valid number greater than 0' };
+    // Method to check if a number is prime
+    @Get('prime/:number')
+    isPrime(@Param('number') number: number): { isPrime: boolean } {
+        if (number <= 1) return { isPrime: false };
+        for (let i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i === 0) {
+                return { isPrime: false };
+            }
         }
-        const sequence = this.generateFibonacci(num);
-        return { sequence };
-    }
-
-    private generateFibonacci(n: number): number[] {
-        const sequence = [0, 1];
-        for (let i = 2; i < n; i++) {
-            sequence.push(sequence[i - 1] + sequence[i - 2]);
-        }
-        return sequence.slice(0, n);
+        return { isPrime: true };
     }
 }
